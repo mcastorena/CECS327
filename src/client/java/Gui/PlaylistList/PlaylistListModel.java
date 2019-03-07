ec376eccb972c53b8309b8f8ef6b0f0d291a3388
David Taitingfong <djtai@protonmail.com>
2019-03-06 21:10:26 -0800

    Document deleting a playlist
    Add Javadocs for all methods pertaining to deleting a playlist; remove commented-out code in the classes

--- src/client/java/Gui/PlaylistList/PlaylistListModel.java
+++ src/client/java/Gui/PlaylistList/PlaylistListModel.java
@@ -10,63 +10,51 @@ import rpc.CECS327InputStream;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
-import java.util.List;
 
 public class PlaylistListModel {
-    HashMap<String,Playlist> playlists = new HashMap<>();
 
-//    List<Playlist> playlists;
-
-//    public List<Playlist> getPlaylists() {
-//        return playlists;
-//    }
-
-//    protected void setPlaylists(final List<Playlist> playlists) {
-//        this.playlists = playlists;
-//    }
-
+    private HashMap<String, Playlist> playlists = new HashMap<>();
 
     public HashMap<String, Playlist> getPlaylists() {
         return playlists;
     }
 
-//    public void setPlaylists(final HashMap<String, Playlist> playlists) {
-//        this.playlists = playlists;
-//    }
-public void setPlaylists(CECS327InputStream is) throws IOException {
-    Gson gson = new Gson();
-    JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+    public void setPlaylists(CECS327InputStream is) throws IOException {
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
 
-    reader.beginArray();
-    while(reader.hasNext())
-    {
-        reader.beginObject();
-        String playlistName = reader.nextName();
-        Playlist p = new Playlist(playlistName);
         reader.beginArray();
-        while(reader.hasNext())
-        {
+        while (reader.hasNext()) {
             reader.beginObject();
-            reader.nextName();
-            p.addToPlaylist(gson.fromJson(reader, CollectionLightWeight.class));
+            String playlistName = reader.nextName();
+            Playlist p = new Playlist(playlistName);
+            reader.beginArray();
+            while (reader.hasNext()) {
+                reader.beginObject();
+                reader.nextName();
+                p.addToPlaylist(gson.fromJson(reader, CollectionLightWeight.class));
+                reader.endObject();
+            }
+            reader.endArray();
             reader.endObject();
+            if (p != null) playlists.put(playlistName, p);
+
         }
         reader.endArray();
-        reader.endObject();
-        if (p != null) playlists.put(playlistName, p);
-
+        UserSession.getCurrentSession().setUserProfile(new Profile(playlists));
     }
-    reader.endArray();
-    UserSession.getCurrentSession().setUserProfile(new Profile(playlists));
-}
 
-    protected void addPlaylist(Playlist p) {
+    void addPlaylist(Playlist p) {
         if (p != null) playlists.put(p.getName(), p);
     }
 
-    protected void removePlaylist(Playlist p) {
-        playlists.remove(p.getName());
+    /**
+     * Deletes the Playlist from the HashMap of all Playlists
+     *
+     * @param playlist - Playlist object to be deleted
+     */
+    void deletePlaylist(Playlist playlist) {
+        playlists.remove(playlist.getName());
     }
 }
