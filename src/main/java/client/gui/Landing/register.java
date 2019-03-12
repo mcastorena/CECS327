2fdba0b7861dbd379f63c640aef777a976584ec3
Miguel <miguel.angel.castorena@gmail.com>
2019-03-12 03:11:13 -0700

    Added RegistrationDispatcher

--- src/main/java/client/gui/Landing/register.java
+++ src/main/java/client/gui/Landing/register.java
@@ -1,15 +1,19 @@
-package client.gui.Landing;
+package Gui.Landing;
 
+import com.google.gson.JsonObject;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import client.model.User;
-import client.rpc.ProxyInterface;
+import rpc.ProxyInterface;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class register {
     private Stage stage;
@@ -22,12 +26,14 @@ public class register {
     private TextField pass;
     @FXML
     private Button signupButton;
+    @FXML
+    private Text registerText;
 
     public register(Stage stage, ProxyInterface proxy) {
         try {
             this.proxy = proxy;
             this.stage = stage;
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("ui/Registration.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/Registration.fxml"));
             loader.setController(this);
             view = loader.load();
         } catch (IOException e) {
@@ -39,16 +45,25 @@ public class register {
         signupButton.setOnMouseClicked(e -> {
             LandingService ls = LandingService.getInstance(proxy);
 
+            // Get username and password
             String username = user.getText();
             String password = pass.getText();
 
-            User u = new User(user.getText(), "", pass.getText());
+            // Create HashMap to hold parameters
+            Map<String, String> params = new HashMap<String, String>();
+            params.put("username", username);
+            params.put("password", password);
 
-            // TODO: REGISTER DISPATCHER
-            //ls.userList.add(u);
-            //ls.usersInfo.put(username+pass, u);
+            // Get and display response and clear entry forms
+            JsonObject response = this.proxy.syncExecution("register", params);
+            String responsePayload = response.get("ret").getAsString();
+            Font myFont = registerText.getFont();
+            registerText.setFont(new Font(myFont.getName(), myFont.getSize()/2));
+            registerText.setText(responsePayload);
+            user.clear();
+            pass.clear();
 
-            stage.close();
+
         });
     }
 
