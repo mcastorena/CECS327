adb6a4e25d27d838f5401323dbc29cf5f07eb47f
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2019-03-12 11:46:24 -0700

    Pushing errorhandling update for incorrect login

--- src/main/java/client/gui/Landing/LandingPresenter.java
+++ src/main/java/client/gui/Landing/LandingPresenter.java
@@ -81,6 +81,8 @@ public class LandingPresenter {
             isAuthorized = landingService.authorizeUser(landingModel.getUsernameInput(), landingModel.getPasswordInput());
         } catch (IOException e) {
             e.printStackTrace();
+        } catch(NullPointerException e){
+            displayError();
         }
 
         if (isAuthorized) {
