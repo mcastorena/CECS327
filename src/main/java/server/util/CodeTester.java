125d3d307748f3549fdfac272975a0c94e0f63f9
Kyle Wu <kylewu2@gmail.com>
2019-04-08 15:38:02 -0700

    Dummy class to test code in

--- src/main/java/server/util/CodeTester.java
+++ src/main/java/server/util/CodeTester.java
@@ -1,4 +1,38 @@
 package server.util;
 
+import server.core.MyTask;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class CodeTester {
+    public static void main(String[] args) throws Exception {
+        try {
+            for (int i = 0; i < 1; i++) {
+//                MyTask task = new MyTask("8727380415757208375/repository/8555781317612585347");
+//                Thread t = new Thread(task);
+//                t.start();
+//                t.join();
+
+//                var data = task.getDatastream();
+//                data.connect();
+
+
+//                Scanner in = new Scanner(data);
+//                in.useDelimiter("\\A");
+//                System.out.println(in.next() + "\n");
+
+                var p = new FileInputStream("8727380415757208375/repository/8555781317612585347");
+                Scanner in = new Scanner(p);
+                in.useDelimiter("\\A");
+                System.out.println(in.next());
+
+
+
+            }
+        } finally {
+            System.out.println("Done.");
+        }
+    }
 }
