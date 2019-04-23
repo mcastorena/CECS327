a1768116f7da25ac5778350f785b593d3705934f
kyleamazza <kyleamazza@gmail.com>
2019-04-23 14:03:42 -0700

    Removed repo files

--- src/main/java/server/chord/MapReduceInterface.java
+++ src/main/java/server/chord/MapReduceInterface.java
@@ -1,4 +1,10 @@
 package server.chord;
 
-public class MapReduceInterface {
+import com.google.gson.JsonObject;
+import java.io.IOException;
+
+
+public interface MapReduceInterface {
+    public void map(String key, JsonObject value, DFS context, String file) throws IOException;
+    public void reduce(String key, JsonObject values, DFS context, String file) throws IOException;
 }
