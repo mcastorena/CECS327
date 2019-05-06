56a015e628becb0f7524eb670fe3eacbcf04cee0
Kyle Wu <kylewu2@gmail.com>
2019-05-06 15:40:01 -0700

    Added drag-and-drop for 'By Artist' display items

--- src/main/java/client/gui/MainDisplay/MainDisplayItem2.java
+++ src/main/java/client/gui/MainDisplay/MainDisplayItem2.java
@@ -1,11 +1,17 @@
 package client.gui.MainDisplay;
 
 import client.app.App;
+import client.data.CollectionFormat;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import client.model.CollectionLightWeight;
 
@@ -81,6 +87,24 @@ public class MainDisplayItem2 {
         });
 
         songPane.setOnMouseClicked(e -> sendPlayRequest());
+
+        songPane.setOnDragDetected(e -> {
+            App.setCursorStyle(Cursor.CLOSED_HAND);
+
+            Dragboard dragboard = songPane.startDragAndDrop(TransferMode.COPY);
+            ClipboardContent content = new ClipboardContent();
+            content.put(CollectionFormat.FORMAT, this.song);
+            dragboard.setContent(content);
+            e.consume();
+
+            // Create a "ghost" image of the pane on drag
+            dragboard.setDragView(
+                    view.snapshot(
+                            new SnapshotParameters(),
+                            new WritableImage(
+                                    songPane.widthProperty().intValue(),
+                                    songPane.heightProperty().intValue())));
+        });
     }
 
     public Parent getView() {
